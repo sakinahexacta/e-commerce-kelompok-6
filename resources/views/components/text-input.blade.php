81839ab691aa053ad73df09e933531b67e26467a
sakinahexacta <excsakinah@gmail.com>
2025-11-29 23:43:50 +0700

    update tampilan dashboard dan profile

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -3,5 +3,3 @@
 <<<<<<< HEAD
 <input @disabled($disabled) {{ $attributes->merge(['class' => 'bg-white-300 border-none focus:border-pink-700 focus:ring-pink-700 rounded-md shadow-sm']) }}>
 =======
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'bg-pink-300 border-none focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
->>>>>>> 85637c0c331299ca6628c810b2040e8fd08cda3b
