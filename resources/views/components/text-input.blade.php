7c96dea8b46fc56b30fedd028f362f6f10cecb30
sakinahexacta <excsakinah@gmail.com>
2025-12-02 22:28:57 +0700

    update home dan category

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,5 +1,3 @@
 @props(['disabled' => false])
 
-<<<<<<< HEAD
 <input @disabled($disabled) {{ $attributes->merge(['class' => 'bg-white-300 border-none focus:border-pink-700 focus:ring-pink-700 rounded-md shadow-sm']) }}>
-=======
